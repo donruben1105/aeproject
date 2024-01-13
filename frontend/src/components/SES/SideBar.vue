8510e0c0cdd9b9461b105b2d953173a5ed7e3ac4
unknown <andres.dev1105@gmail.com>
2024-01-13 19:07:18 +0800

    added logout

--- frontend/src/components/SES/SideBar.vue
+++ frontend/src/components/SES/SideBar.vue
@@ -61,11 +61,30 @@
             </a>
           </li>
         </router-link>
+        <router-link to="/" custom v-slot="{ href, navigate }" @click="signOut">
+          <li>
+            <a :href="href" @click="navigate" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-red-500">
+              <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class='bx bx-log-out'></i></span>
+              <span class="text-sm font-medium">Logout</span>
+            </a>
+          </li>
+        </router-link>
     </ul>
   </div>
 </div>
 </template>
 
 <script setup lang="ts">
+import axios from 'axios'
+
+const signOut = async () => {
+  try {
+    await axios.post('logout', null, {
+      withCredentials: true,
+    })
+  } catch (error) {
+    console.error(error)
+  }
+}
 
 </script>
